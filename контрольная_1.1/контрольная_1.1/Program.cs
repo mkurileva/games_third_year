0a427ab95075d9e0d7a789cc1797d6121c6798cb
mkurileva <164143914+mkurileva@users.noreply.github.com>
2024-10-13 13:49:31 +0300

    Add files via upload

---
+++ контрольная_1.1/контрольная_1.1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace zadanie1
+{
+    internal class Program
+    {
+        static double Factorial(int n)
+        {
+
+            if (n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return Factorial(n - 1) * n;
+            }
+
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число x");
+            double x = Convert.ToUInt32(Console.ReadLine());
+            double rez = 0;
+            double znak = 1;
+            for (int i = 1; i < 14; i = i + 2)
+            {
+                rez = rez + znak * (Math.Pow(x, i) / Factorial(i));
+                znak = znak * (-1);
+            }
+            Console.Write($"Результат равен {rez}");
+        }
+    }
+}
