0f367d55163c7bd92ca0376d90e37f8270bd807b
mkurileva <164143914+mkurileva@users.noreply.github.com>
2024-11-05 19:09:00 +0300

    добавила дз 1.1, 1.2, 1.3

---
+++ дз 1.1/дз 1.1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+        int limit;
+
+        if (!int.TryParse(Console.ReadLine(), out limit) || limit < 2)
+        {
+            Console.WriteLine("Пожалуйста, введите целое число, большее или равное 2.");
+            return;
+        }
+
+        bool[] isPrime = new bool[limit + 1];
+        for (int i = 2; i <= limit; i++)
+            isPrime[i] = true;
+
+        for (int i = 2; i * i <= limit; i++)
+        {
+            if (isPrime[i])
+            {
+                for (int j = i * i; j <= limit; j += i)
+                {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        Console.WriteLine("Простые числа, не превосходящие " + limit + ":");
+        for (int i = 2; i <= limit; i++)
+        {
+            if (isPrime[i])
+                Console.Write(i + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
---
+++ дз 1.2/дз 1.2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите делимое a: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите делитель b: ");
+        int b = int.Parse(Console.ReadLine());
+
+        if (b == 0)
+        {
+            Console.WriteLine("Ошибка: делитель не может быть равен нулю.");
+            return;
+        }
+
+        int q = FindQuotient(a, b);
+        Console.WriteLine("Неполное частное от деления " + a + " на " + b + " равно: " + q);
+    }
+
+    static int FindQuotient(int a, int b)
+    {
+        int q = 0;
+        int absA = Math.Abs(a);
+        int absB = Math.Abs(b);
+
+        while (absA >= absB * (q + 1))
+        {
+            q++;
+        }
+
+        if ((a < 0 && b > 0) || (a > 0 && b < 0))
+            q = -q;
+
+        return q;
+    }
+}
