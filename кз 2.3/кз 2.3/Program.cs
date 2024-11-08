04bafdea1b55de3a8c1d3efcdaf49bb5ffda7092
mkurileva <164143914+mkurileva@users.noreply.github.com>
2024-11-08 21:54:58 +0300

    добавила кз 2.3 и 2.4

---
+++ кз 2.3/кз 2.3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число n: ");
+        if (int.TryParse(Console.ReadLine(), out int n) && n > 0)
+        {
+            int count = 0;
+
+            while (n != 1)
+            {
+                if (n % 2 == 0)
+                {
+                    n /= 2;
+                }
+                else
+                {
+                    n = 3 * n + 1;
+                }
+                count++;
+            }
+
+            Console.WriteLine($"Количество замен для достижения 1: {count}");
+        }
+        else
+        {
+            Console.WriteLine("Введите положительное целое число.");
+        }
+    }
+}
---
+++ кз 2.4/кз 2.4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите количество элементов массива: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int[] array = new int[n];
+        Console.WriteLine("Введите элементы массива:");
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Элемент {i + 1}: ");
+            array[i] = int.Parse(Console.ReadLine());
+        }
+
+        BubbleSort(array);
+
+        Console.WriteLine("Отсортированный массив:");
+        foreach (int item in array)
+        {
+            Console.Write(item + " ");
+        }
+    }
+
+    static void BubbleSort(int[] array)
+    {
+        int n = array.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (array[j] > array[j + 1])
+                {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
+
