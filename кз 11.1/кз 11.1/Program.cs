36e60399a57102a4b373d680ee27053b1efbe8b6
mkurileva <164143914+mkurileva@users.noreply.github.com>
2024-11-16 11:04:18 +0300

    кз 11.1

---
+++ кз 11.1/кз 11.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = "f.txt";
+
+        using (StreamWriter writer = new StreamWriter(filePath))
+        {
+            Console.WriteLine($"Текущая директория: {Environment.CurrentDirectory}");
+            Console.WriteLine("Начинаю запись...");
+            writer.WriteLine("x sin(x)");
+
+            for (double x = 0; x <= 1; x += 0.1)
+            {
+                double sinValue = Math.Sin(x); 
+                writer.WriteLine($"{x:F1} {sinValue:F4}"); 
+            }
+        }
+
+        Console.WriteLine($"Таблица значений sin(x) сохранена в файл \"{filePath}\".");
+    }
+}
+
