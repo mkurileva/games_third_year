5b9bb776e20d0b76a175bad336fa23ac6dbf454d
mkurileva <164143914+mkurileva@users.noreply.github.com>
2024-11-29 11:58:23 +0300

    кдз 7 и кдз 11

---
+++ кдз 11 2048/кдз 11 2048/Program.cs
@@ -0,0 +1,174 @@
+﻿using System;
+
+class Game2048
+{
+    const int Size = 4;
+    static int[,] board = new int[Size, Size];
+    static Random rand = new Random();
+    static int score = 0;
+
+    static void Main()
+    {
+        SpawnTile();
+        SpawnTile();
+        while (true)
+        {
+            Console.Clear();
+            PrintBoard();
+            if (IsGameOver())
+            {
+                Console.WriteLine("Гамовер! Твой счёт: " + score);
+                break;
+            }
+
+            ConsoleKey key = Console.ReadKey(true).Key;
+            bool moved = key switch
+            {
+                ConsoleKey.UpArrow => MoveUp(),
+                ConsoleKey.DownArrow => MoveDown(),
+                ConsoleKey.LeftArrow => MoveLeft(),
+                ConsoleKey.RightArrow => MoveRight(),
+                _ => false
+            };
+
+            if (moved)
+            {
+                SpawnTile();
+            }
+        }
+    }
+
+    static void SpawnTile()
+    {
+        var emptyCells = new System.Collections.Generic.List<(int, int)>();
+        for (int i = 0; i < Size; i++)
+            for (int j = 0; j < Size; j++)
+                if (board[i, j] == 0)
+                    emptyCells.Add((i, j));
+
+        if (emptyCells.Count > 0)
+        {
+            var (x, y) = emptyCells[rand.Next(emptyCells.Count)];
+            board[x, y] = rand.Next(10) < 9 ? 2 : 4; //вероятность
+        }
+    }
+
+    static void PrintBoard()
+    {
+        Console.WriteLine("Счёт: " + score);
+        for (int i = 0; i < Size; i++)
+        {
+            for (int j = 0; j < Size; j++)
+            {
+                Console.Write(board[i, j] == 0 ? ".\t" : board[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static bool MoveLeft()
+    {
+        bool moved = false;
+        for (int i = 0; i < Size; i++)
+        {
+            int[] row = new int[Size];
+            int pos = 0;
+            for (int j = 0; j < Size; j++)
+            {
+                if (board[i, j] != 0)
+                {
+                    row[pos++] = board[i, j];
+                }
+            }
+
+            for (int j = 0; j < Size - 1; j++)
+            {
+                if (row[j] != 0 && row[j] == row[j + 1])
+                {
+                    row[j] *= 2;
+                    score += row[j];
+                    row[j + 1] = 0;
+                }
+            }
+
+            pos = 0;
+            for (int j = 0; j < Size; j++)
+            {
+                if (row[j] != 0)
+                {
+                    if (board[i, pos] != row[j])
+                    {
+                        moved = true;
+                    }
+                    board[i, pos++] = row[j];
+                }
+            }
+
+            while (pos < Size)
+            {
+                if (board[i, pos] != 0)
+                {
+                    moved = true;
+                }
+                board[i, pos++] = 0;
+            }
+        }
+        return moved;
+    }
+
+    static bool MoveRight()
+    {
+        RotateBoard();
+        RotateBoard();
+        bool moved = MoveLeft();
+        RotateBoard();
+        RotateBoard();
+        return moved;
+    }
+
+    static bool MoveUp()
+    {
+        RotateBoard();
+        RotateBoard();
+        RotateBoard();
+        bool moved = MoveLeft();
+        RotateBoard();
+        return moved;
+    }
+
+    static bool MoveDown()
+    {
+        RotateBoard();
+        bool moved = MoveLeft();
+        RotateBoard();
+        RotateBoard();
+        RotateBoard();
+        return moved;
+    }
+
+    static void RotateBoard()
+    {
+        int[,] temp = new int[Size, Size];
+        for (int i = 0; i < Size; i++)
+            for (int j = 0; j < Size; j++)
+                temp[i, j] = board[Size - j - 1, i];
+
+        Array.Copy(temp, board, Size * Size);
+    }
+
+    static bool IsGameOver()
+    {
+        for (int i = 0; i < Size; i++)
+            for (int j = 0; j < Size; j++)
+            {
+                if (board[i, j] == 0)
+                    return false;
+                if (j < Size - 1 && board[i, j] == board[i, j + 1])
+                    return false;
+                if (i < Size - 1 && board[i, j] == board[i + 1, j])
+                    return false;
+            }
+        return true;
+    }
+}
+
---
+++ кдз 7 лабиринт/кдз 7 лабиринт/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace MazeGame
+{
+    class Program
+    {
+        static char[,] maze;  // Лабиринт
+        static int playerX, playerY;  // Позиция игрока
+        static int exitX, exitY;  // Позиция выхода
+
+        static void Main(string[] args)
+        {
+            int width = 21;  // Ширина лабиринта (должна быть нечетной)
+            int height = 11; // Высота лабиринта (должна быть нечетной)
+
+            maze = GenerateMaze(width, height);  // Генерация лабиринта
+            playerX = 1;
+            playerY = 1;
+            exitX = width - 2;
+            exitY = height - 2;
+
+            // Основной игровой цикл
+            while (true)
+            {
+                Console.Clear();
+                PrintMaze();
+                MovePlayer();
+
+                if (playerX == exitX && playerY == exitY)
+                {
+                    Console.Clear();
+                    PrintMaze();
+                    Console.WriteLine("Поздравляем! Вы нашли выход!");
+                    break;
+                }
+            }
+        }
+
+        // Генерация лабиринта алгоритмом Sidewinder
+        static char[,] GenerateMaze(int width, int height)
+        {
+            char[,] maze = new char[height, width];
+
+            // Заполняем лабиринт стенами
+            for (int y = 0; y < height; y++)
+                for (int x = 0; x < width; x++)
+                    maze[y, x] = (x % 2 == 1 && y % 2 == 1) ? ' ' : '#';
+
+            Random rand = new Random();
+
+            // Генерация построчно
+            for (int y = 1; y < height; y += 2)
+            {
+                int runStart = 1; // Начало рунки
+
+                for (int x = 1; x < width; x += 2)
+                {
+                    if (x + 2 < width && (y == 1 || rand.Next(2) == 0))
+                    {
+                        // Продолжить рунку вправо
+                        maze[y, x + 1] = ' ';
+                    }
+                    else
+                    {
+                        // Завершить рунку: соединить вверх
+                        int runEnd = rand.Next(runStart, x + 1);
+                        if (y > 1)
+                            maze[y - 1, runEnd] = ' ';
+                        runStart = x + 2; // Начать новую рунку
+                    }
+                }
+            }
+
+            // Обеспечиваем наличие старта и выхода
+            maze[1, 1] = ' ';
+            maze[height - 2, width - 2] = ' ';
+            return maze;
+        }
+
+        // Печать лабиринта
+        static void PrintMaze()
+        {
+            for (int y = 0; y < maze.GetLength(0); y++)
+            {
+                for (int x = 0; x < maze.GetLength(1); x++)
+                {
+                    if (x == playerX && y == playerY)
+                        Console.Write('C'); // Игрок - character
+                    else if (x == exitX && y == exitY)
+                        Console.Write('E'); // Выход
+                    else
+                        Console.Write(maze[y, x]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        // Перемещение игрока
+        static void MovePlayer()
+        {
+            ConsoleKey key = Console.ReadKey(true).Key;
+
+            int newX = playerX, newY = playerY;
+
+            if (key == ConsoleKey.UpArrow && playerY > 0 && maze[playerY - 1, playerX] != '#')
+                newY--;
+            else if (key == ConsoleKey.DownArrow && playerY < maze.GetLength(0) - 1 && maze[playerY + 1, playerX] != '#')
+                newY++;
+            else if (key == ConsoleKey.LeftArrow && playerX > 0 && maze[playerY, playerX - 1] != '#')
+                newX--;
+            else if (key == ConsoleKey.RightArrow && playerX < maze.GetLength(1) - 1 && maze[playerY, playerX + 1] != '#')
+                newX++;
+
+            // Если движение возможно, обновляем позицию игрока
+            if (maze[newY, newX] != '#')
+            {
+                playerX = newX;
+                playerY = newY;
+            }
+        }
+    }
+}
